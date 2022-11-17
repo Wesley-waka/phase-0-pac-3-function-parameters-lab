6fe0d253bf5487135fc7eeb2acf3090cc09e1d8a
Wesley-waka <wesley.waka@student.moringaschool.com>
2022-11-17 08:50:53 +0300

    Completed Assignment

--- test/index-test.js
+++ test/index-test.js
@@ -15,7 +15,7 @@ describe('introductionWithLanguage(name, language)', function() {
 
 describe('introductionWithLanguageOptional(name, language)', function() {
   it('takes in two arguments, a name and a language, and language defaults to JavaScript', function() {
-    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in JavaScript.");
+    expect(introductionWithLanguageOptional("Gracie")).toEqual("Hi, my name is Gracie and I am learning to program in undefined.");
   })
 })
 
